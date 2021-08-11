5cf3200c26745be7606511306617de95a65d7ebd
pathfinder888 <noffkuma@gmail.com>
2021-08-11 10:39:22 +0900

    my first app,filter

--- bookpage/views.py
+++ bookpage/views.py
@@ -12,20 +12,43 @@ monthnow = dtnow.month
 class BookList(ListView):
     model = Manga
     template_name = 'bookpage/book_list.html'
-
-    queryset = Manga.objects.filter(publish_date__month=monthnow).order_by('publish_date')
+    
+    
     
     def get_queryset(self):
+        queryset = Manga.objects.filter(publish_date__month=monthnow).order_by('publish_date')
         q_word = self.request.GET.get('query')
-        f_word = self.request.POST.getlist('filter')
+        f_word = self.request.GET.getlist('filter')
 
         if q_word:
             object_list = Manga.objects.filter(
                 Q(title__icontains=q_word) | Q(author__icontains=q_word) 
                 | Q(publisher__icontains=q_word)).filter(publish_date__month=monthnow).order_by('publish_date')
         elif f_word:
-            object_list = (Manga.objects.filter(Q(publisher__icontains=f_word))
-            .filter(publish_date__month=monthnow).order_by('publish_date'))
+            if len(f_word) == 1:
+                object_list = (Manga.objects.filter(Q(publisher__icontains=f_word[0]))
+                .filter(publish_date__month=monthnow).order_by('publish_date'))
+            elif len(f_word) == 2:
+                object_list = (Manga.objects.filter(
+                    Q(publisher__icontains=f_word[0]) | Q(publisher__icontains=f_word[1]))
+                .filter(publish_date__month=monthnow).order_by('publish_date'))
+            elif len(f_word) == 3:
+                object_list = (Manga.objects.filter(
+                    Q(publisher__icontains=f_word[0]) | Q(publisher__icontains=f_word[1])
+                     | Q(publisher__icontains=f_word[2]))
+                .filter(publish_date__month=monthnow).order_by('publish_date'))
+            elif len(f_word) == 4:
+                object_list = (Manga.objects.filter(
+                    Q(publisher__icontains=f_word[0]) | Q(publisher__icontains=f_word[1])
+                     | Q(publisher__icontains=f_word[2]) | Q(publisher__icontains=f_word[3]))
+                .filter(publish_date__month=monthnow).order_by('publish_date'))
+            elif len(f_word) == 5:
+                object_list = (Manga.objects.filter(
+                    Q(publisher__icontains=f_word[0]) | Q(publisher__icontains=f_word[1])
+                     | Q(publisher__icontains=f_word[2]) | Q(publisher__icontains=f_word[4])
+                    | Q(publisher__icontains=f_word[5]))
+                .filter(publish_date__month=monthnow).order_by('publish_date'))            
+
         else:
             object_list = Manga.objects.filter(publish_date__month=monthnow).order_by('publish_date')
 
