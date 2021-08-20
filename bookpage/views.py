2b8a9d573811700cb79ebfec3d745da58ada3b76
pathfinder888 <noffkuma@gmail.com>
2021-08-20 09:27:14 +0900

    my first app,filter&css

--- bookpage/views.py
+++ bookpage/views.py
@@ -3,6 +3,7 @@ from .models import Manga
 import datetime
 from django.views.generic import ListView
 from django.db.models import Q
+import calendar
 # Create your views here.
 
 
@@ -19,11 +20,14 @@ class BookList(ListView):
         queryset = Manga.objects.filter(publish_date__month=monthnow).order_by('publish_date')
         q_word = self.request.GET.get('query')
         f_word = self.request.GET.getlist('filter')
+        d_word = self.request.GET.get('day')
+        m_word = self.request.GET.get('month')
 
         if q_word:
             object_list = Manga.objects.filter(
                 Q(title__icontains=q_word) | Q(author__icontains=q_word) 
                 | Q(publisher__icontains=q_word)).filter(publish_date__month=monthnow).order_by('publish_date')
+        
         elif f_word:
             if len(f_word) == 1:
                 object_list = (Manga.objects.filter(Q(publisher__icontains=f_word[0]))
@@ -49,11 +53,32 @@ class BookList(ListView):
                     | Q(publisher__icontains=f_word[5]))
                 .filter(publish_date__month=monthnow).order_by('publish_date'))            
 
+        elif d_word:
+            object_list = (Manga.objects.filter(Q(publish_date__day__gte=int(d_word))).
+                filter(publish_date__month=monthnow).order_by('publish_date'))
+
+        elif m_word:
+            object_list = (Manga.objects.filter(Q(publish_date__month=int(m_word))).order_by('publish_date'))
+
         else:
             object_list = Manga.objects.filter(publish_date__month=monthnow).order_by('publish_date')
-
+        
         return object_list
     
+    def get_context_data(self,**kwargs):
+        day = [x for x in range(1,calendar.monthrange(dtnow.year,dtnow.month)[1]+1)]
+        month = [monthnow-1,monthnow,monthnow+1]
+
+
+        context = super().get_context_data(**kwargs)
+        context['days'] = day
+        context['months'] = month
+    
+        return context
+
+
+
+    
 
     
 
@@ -61,21 +86,3 @@ class BookList(ListView):
 
 
 
-# def book_list(request):
-    
-#     dtnow = datetime.datetime.now()
-#     monthnow = dtnow.month
-    
-#     mangaslist = Manga.objects.filter(publish_date__month=monthnow).order_by('publish_date')
-    
-#     return render(request,'bookpage/book_list.html',{'mangaslist':mangaslist})
-
-
-
-
-
-# class MypageList(ListView):
-#     model = Manga
-#     template_name = 'mypage.html'
-    # def mypage(request):
-    # return render(request,'bookpage/mypage.html',{});
